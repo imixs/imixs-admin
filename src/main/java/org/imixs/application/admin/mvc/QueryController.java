edb16efbfdce2f74b24a8f5faf6375552ed8f5cf
Ralph Soika <ralph.soika@imixs.com>
2018-06-20 09:05:56 +0200

    refactoring

--- src/main/java/org/imixs/application/admin/mvc/QueryController.java
+++ src/main/java/org/imixs/application/admin/mvc/QueryController.java
@@ -4,7 +4,6 @@ import java.io.Serializable;
 import java.util.List;
 import java.util.logging.Logger;
 
-import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.mvc.annotation.Controller;
