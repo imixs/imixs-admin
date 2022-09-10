e298d53876637d29aea98a9d5f955901fe4e529e
Ralph Soika <ralph.soika@imixs.com>
2022-09-10 17:10:14 +0200

    added adminP
    Issue #65

--- src/main/java/org/imixs/application/admin/AdminPController.java
+++ src/main/java/org/imixs/application/admin/AdminPController.java
@@ -1,10 +1,16 @@
 package org.imixs.application.admin;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.logging.Logger;
 
+import org.imixs.melman.RestAPIException;
 import org.imixs.melman.WorkflowClient;
+import org.imixs.workflow.ItemCollection;
+import org.imixs.workflow.xml.XMLDataCollection;
+import org.imixs.workflow.xml.XMLDataCollectionAdapter;
 
 import jakarta.enterprise.context.ConversationScoped;
 import jakarta.inject.Inject;
@@ -20,6 +26,10 @@ import jakarta.inject.Named;
 @ConversationScoped
 public class AdminPController implements Serializable {
 
+    public static final String JOB_RENAME_USER = "RENAME_USER";
+    public static final String JOB_REBUILD_INDEX = "JOB_REBUILD_INDEX";
+    public static final String JOB_UPGRADE = "UPGRADE";
+
     private static final long serialVersionUID = 1L;
 
     private static Logger logger = Logger.getLogger(AdminPController.class.getName());
@@ -33,6 +43,7 @@ public class AdminPController implements Serializable {
     private String renameUserFrom;
     private String renameUserTo;
     private boolean renameFullReplace;
+    List<ItemCollection> jobs = null;
 
     @Inject
     ConnectionController connectionController;
@@ -120,21 +131,79 @@ public class AdminPController implements Serializable {
      */
     public void executeJob() {
         logger.info("...starting Admin-P");
-        int updates = 0;
 
-        logController.reset();
-        // try {
-        WorkflowClient workflowClient = connectionController.getWorkflowClient();
-        if (workflowClient != null) {
-            // set items!
+        reset();
+        try {
+            WorkflowClient workflowClient = connectionController.getWorkflowClient();
+            if (workflowClient != null) {
 
-            logController.info("...admin-p started");
+                ItemCollection job = new ItemCollection();
+                job.replaceItemValue("job", jobType);
+                job.replaceItemValue("numinterval", interval); // seconds
+                job.replaceItemValue("blocksize", blockSize);
+
+                if (JOB_REBUILD_INDEX.equals(getJobType())) {
+                }
+
+                if (JOB_RENAME_USER.equals(getJobType())) {
+                    job.replaceItemValue("typelist", filter);
+                    job.replaceItemValue("datfrom", dateFrom);
+                    job.replaceItemValue("datto", dateTo);
+
+                    job.replaceItemValue("namfrom", renameUserFrom);
+                    job.replaceItemValue("namto", renameUserTo);
+                    job.replaceItemValue("keyreplace", renameFullReplace);
+
+                }
+                if (JOB_UPGRADE.equals(getJobType())) {
+                    job.replaceItemValue("typelist", filter);
+                    job.replaceItemValue("datfrom", dateFrom);
+                    job.replaceItemValue("datto", dateTo);
+                }
+
+                // post Admin-P job
+                workflowClient.createAdminPJob(job);
+                logController.info("...admin-p job '" + getJobType() + "' started");
+                logController.info("...block-size=" + blockSize);
+                logController.info("...interval=" + interval + "sec");
+
+            }
+        } catch (RestAPIException e) {
+            logController.warning("Rest API Error: " + e.getMessage());
 
         }
-//        } catch (RestAPIException e) {
-//            logController.warning("Rest API Error: " + e.getMessage());
-//
-//        }
+    }
+
+    /**
+     * Reset job list
+     */
+    public void reset() {
+        logController.reset();
+        jobs = null;
+    }
+
+    /**
+     * Returns the current job list
+     *
+     * @return
+     */
+    public List<ItemCollection> getJobs() {
+        if (jobs == null) {
+            logger.info("compute job list...");
+            jobs = new ArrayList<ItemCollection>();
+            try {
+                WorkflowClient workflowClient = connectionController.getWorkflowClient();
+                if (workflowClient != null) {
+                    XMLDataCollection jobList = workflowClient.getCustomResourceXML("/adminp/jobs");
+                    jobs = XMLDataCollectionAdapter.putDataCollection(jobList);
+                }
+            } catch (RestAPIException e) {
+                logController.warning("Rest API Error: " + e.getMessage());
+
+            }
+        }
+
+        return jobs;
     }
 
 }
