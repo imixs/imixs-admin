91489e3f68b33947ab07bc304236204d8dcb7dcf
Ralph Soika <ralph.soika@imixs.com>
2022-09-13 20:34:34 +0200

    fixed startscript
    Issue #66

--- wildfly_start.sh
+++ wildfly_start.sh
@@ -8,17 +8,16 @@
 #                                                                                       #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-
-if [ ! -f /opt/wildfly/.wildfly_admin_created ]; then
-  /opt/wildfly/wildfly_add_admin_user.sh
-fi
-
+# disable admin user
+#if [ ! -f /opt/wildfly/.wildfly_admin_created ]; then
+#  /opt/wildfly/wildfly_add_admin_user.sh
+#fi
 
 # test for starting in debug mode...
 if [ "$DEBUG" == "true" ]; then    
   echo "starting wildfly in debug mode..."
-  exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug *:8787 --server-config=standalone.xml
+  exec /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug *:8787
 else
   echo "starting wildfly..."
-  exec /opt/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
+  exec /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
 fi;
