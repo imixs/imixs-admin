48a751c2b078a794e491f89f7d16b3e31b82f9fe
Ralph Soika <ralph.soika@imixs.com>
2025-11-21 12:44:49 +0100

    implemented monitoring ui (Issue #82)

--- src/main/java/org/imixs/application/admin/SchedulerController.java
+++ src/main/java/org/imixs/application/admin/SchedulerController.java
@@ -1,14 +1,19 @@
 package org.imixs.application.admin;
 
 import java.io.Serializable;
-import java.io.UnsupportedEncodingException;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import org.imixs.melman.RestAPIException;
 import org.imixs.melman.WorkflowClient;
 import org.imixs.workflow.ItemCollection;
+import org.imixs.workflow.xml.XMLDataCollection;
+import org.imixs.workflow.xml.XMLDataCollectionAdapter;
+import org.imixs.workflow.xml.XMLDocument;
+import org.imixs.workflow.xml.XMLDocumentAdapter;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.enterprise.context.ConversationScoped;
@@ -43,8 +48,12 @@ public class SchedulerController implements Serializable {
 
     @PostConstruct
     public void init() {
-        schedulerConfig = loadSchedulerConfiguration();
+        schedulerConfig = load();
 
+    }
+
+    public ItemCollection getSchedulerConfig() {
+        return schedulerConfig;
     }
 
     /**
@@ -82,27 +91,37 @@ public class SchedulerController implements Serializable {
     /**
      * This method starts a bulk update
      */
-    public void startScheduler() {
-        logger.info("...starting Worklfow Scheduler");
-        int deletions = 0;
+    public void start() {
+        logger.info("├── Starting Workflow Scheduler...");
+
         logController.reset();
         try {
             WorkflowClient workflowClient = connectionController.getWorkflowClient();
             if (workflowClient != null) {
-                // set items!
-                workflowClient.setItems("");
-                searchController.reset();
-                List<ItemCollection> documents = searchController.getSearchResult();
-                logController.info("..." + documents.size() + " documents selected for bulk delete...");
+                schedulerConfig.setItemValue("_scheduler_enabled", true);
+                XMLDocument xmlWorkitem = XMLDocumentAdapter.getDocument(schedulerConfig);
+                XMLDataCollection result = workflowClient.postXMLDocument("/scheduler/", xmlWorkitem);
+                schedulerConfig = XMLDataCollectionAdapter.putDataCollection(result).get(0);
+            }
+        } catch (RestAPIException e) {
+            logController.warning("Rest API Error: " + e.getMessage());
+        }
+    }
 
-                // iterate over all documents....
-                for (ItemCollection document : documents) {
-                    // delete workitem
-                    workflowClient.deleteDocument(document.getUniqueID());
-                    deletions++;
-                }
-                logController.info("...bulk delete finished: " + deletions + " deletions.");
-                searchController.reset();
+    /**
+     * This method starts a bulk update
+     */
+    public void stop() {
+        logger.info("├── Starting Workflow Scheduler...");
+
+        logController.reset();
+        try {
+            WorkflowClient workflowClient = connectionController.getWorkflowClient();
+            if (workflowClient != null) {
+                schedulerConfig.setItemValue("_scheduler_enabled", false);
+                XMLDocument xmlWorkitem = XMLDocumentAdapter.getDocument(schedulerConfig);
+                XMLDataCollection result = workflowClient.postXMLDocument("/scheduler/", xmlWorkitem);
+                schedulerConfig = XMLDataCollectionAdapter.putDataCollection(result).get(0);
             }
         } catch (RestAPIException e) {
             logController.warning("Rest API Error: " + e.getMessage());
@@ -115,37 +134,71 @@ public class SchedulerController implements Serializable {
      *
      * @return true if api call was successful
      */
-    private ItemCollection loadSchedulerConfiguration() {
-
+    public ItemCollection load() {
+        schedulerConfig = null;
+        logger.info("├── Load Workflow Scheduler configuration");
         WorkflowClient workflowClient = connectionController.getWorkflowClient();
         if (workflowClient != null) {
-            logger.info("├── Load Workflow Scheduler configuration");
+
             try {
-                String query = "(type:scheduler) AND (name:org.imixs.workflow.scheduler)";
-                List<ItemCollection> searchResult = workflowClient.searchDocuments(query);
-                if (searchResult == null || searchResult.size() == 0) {
-                    logger.info("│   ├──  Create new Workflow Scheduler default configuration");
-                    schedulerConfig = new ItemCollection();
-                    schedulerConfig.setType("scheduler");
-                    schedulerConfig.setItemValue("name", "org.imixs.workflow.scheduler")
-                            .setItemValue("_scheduler_class", "org.imixs.workflow.engine.WorkflowScheduler")
-                            .setItemValue("_scheduler_definition", "hour=*").setItemValue("_scheduler_enabled", false);
+
+                List<ItemCollection> result = workflowClient
+                        .getCustomResource("scheduler/org.imixs.workflow.scheduler");
+                // a valid result object contains the item lucence.fulltextFieldList'
+                if (result == null || result.size() == 0) {
+                    logController.info("│   ├──  Create new Workflow Scheduler default configuration");
+                    createDefaultConfiguration();
                 } else {
-                    schedulerConfig = searchResult.get(0);
-
-                    if (searchResult.size() > 1) {
-                        logger.warning("│   ├──  ⚠️ More then one Workflow Scheduler configuration found!");
-                    }
+                    schedulerConfig = result.get(0);
                 }
+            } catch (RestAPIException e) {
+                logController.info("├── Failed to load scheduler configuration - create default configuration");
+                createDefaultConfiguration();
 
-                return schedulerConfig;
-
-            } catch (RestAPIException | UnsupportedEncodingException e) {
-
-                logController.severe("├── Failed to load scheduler configuration: " + e.getMessage());
             }
         }
-        return null;
+
+        return schedulerConfig;
+    }
+
+    /**
+     * Helper method to create an empty default config
+     */
+    private void createDefaultConfiguration() {
+        schedulerConfig = new ItemCollection();
+        schedulerConfig.setType("scheduler");
+        schedulerConfig.setItemValue("name", "org.imixs.workflow.scheduler")
+                .setItemValue("_scheduler_class", "org.imixs.workflow.engine.WorkflowScheduler")
+                .setItemValue("_scheduler_definition", "hour=*").setItemValue("_scheduler_enabled", false);
+
+    }
+
+    /**
+     *
+     * converts time (in milliseconds) to human-readable format "<dd:>hh:mm:ss"
+     *
+     * @return
+     */
+    public String millisToShortDHMS(int duration) {
+        boolean debug = logger.isLoggable(Level.FINE);
+        if (debug) {
+            logger.log(Level.FINEST, "......confert ms {0}", duration);
+        }
+        String res = "";
+        long days = TimeUnit.MILLISECONDS.toDays(duration);
+        long hours = TimeUnit.MILLISECONDS.toHours(duration)
+                - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(duration));
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(duration)
+                - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(duration));
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(duration)
+                - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(duration));
+        if (days == 0) {
+            res = String.format("%d hours, %d minutes, %d seconds", hours, minutes, seconds);
+        } else {
+            res = String.format("%d days, %d hours, %d minutes, %d seconds", days, hours, minutes, seconds);
+        }
+        return res;
+
     }
 
 }
